29133219763cb0dd962cb85a44a4e148a2706d9f
mostlyunoriginal <lance.couzens@gmail.com>
2025-03-18 16:12:38 -0600

    code updates

--- Scratch.R
+++ Scratch.R
@@ -1,4 +1,22 @@
 library(tidyverse)
+library(dtplyr,warn.conflicts=F)
+library(hms)
 
-filter(mtcars,cyl==6) %>% nrow()
-filter(mtcars,hp>150) %>% nrow()
+start<-Sys.time()
+
+big<-data.table::fread("big.csv")
+
+varnames<-setdiff(colnames(big),"id")
+
+rows_dt<-lazy_dt(big) %>%
+  group_by(id) %>%
+  summarize(across(all_of(varnames),mean),.groups="keep") %>%
+  filter(if_any(all_of(varnames),~.x>.4)) %>%
+  collect() %>%
+  nrow()
+
+end<-Sys.time()
+
+elapsed<-(end-start)
+
+print(str_glue("{rows_dt} rows returned\nelapsed time for query: {as_hms(elapsed)}"))
